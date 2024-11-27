a2cce8d7080d9d556c5a35ea9f83082208b2caeb
LightTMer <saniaersh@gmail.com>
2024-11-27 16:27:31 +0500

    fix consumer

--- consumer.py
+++ consumer.py
@@ -43,37 +43,38 @@ def get_internal_links(url):
     
     return internal_links
 
-def callback(body):
+def callback(ch, method, properties, body):
     url = body.decode()
     print(f'Обрабатывается: {url}')
-    try:
-        get_internal_links(url)
-    except Exception as e:
-        print(f'Ошибка при обработке URL {url}: {e}')
+    get_internal_links(url)
+    
 
 def main():
-
+   
     rabbitmq_host = os.getenv('RABBITMQ_HOST', 'localhost')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
     channel = connection.channel()
 
     channel.queue_declare(queue='links')
 
-    channel.basic_consume(queue='links', on_message_callback=callback, auto_ack=True)
-
     print('Ожидание сообщений. Нажмите Ctrl+C для выхода.')
 
-    while not stop_consuming:
-        try:
-            channel.start_consuming()
-        except pika.exceptions.AMQPConnectionError:
-            print("Проблема с подключением к RabbitMQ. Подключаемся повторно...")
-            time.sleep(5)
-        except Exception as e:
-            print(f'Ошибка: {e}')
+    try:
+        while not stop_consuming:
+            method_frame, header_frame, body = channel.basic_get(queue='links', auto_ack=True)
+            if body:
+                callback(channel, method_frame, header_frame, body)
+            else:
+                time.sleep(1)  
+    except pika.exceptions.AMQPConnectionError:
+        print("Проблема с подключением к RabbitMQ. Повторное подключение...")
+        time.sleep(5)
+    except Exception as e:
+        print(f'Ошибка: {e}')
+    finally:
+        print("Закрытие соединения...")
+        connection.close()
 
-    channel.stop_consuming()
-    connection.close()
 
 if __name__ == "__main__":
     main()
